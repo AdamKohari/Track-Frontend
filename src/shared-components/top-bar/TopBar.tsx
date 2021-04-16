5d3a1f96458165420fbdb62ecb411e5ff307f00c
Adam Kohari <Adam_Kohari@epam.com>
2021-04-16 15:22:08 +0200

    Top bar

--- src/shared-components/top-bar/TopBar.tsx
+++ src/shared-components/top-bar/TopBar.tsx
@@ -1,14 +1,16 @@
 import './TopBar.scss';
-import {AssessmentRounded} from "@material-ui/icons";
+import {ArrowBack, AssessmentRounded, ExitToAppOutlined} from "@material-ui/icons";
 
 function TopBar() {
     return (
         <div className="top-bar">
+            <ArrowBack className="svg-button-disabled"/>
             <div className="logo">
                 <AssessmentRounded />
                 <span>Track</span>
                 <span>You</span>
             </div>
+            <ExitToAppOutlined />
         </div>
     )
 }
