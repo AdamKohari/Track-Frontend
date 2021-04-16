de404dd02c6dd0737791538a68ca1196c972b502
Adam Kohari <Adam_Kohari@epam.com>
2021-04-16 16:37:10 +0200

    Loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {lazy, Suspense} from 'react';
 import './App.scss';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
+import LoadingFullPage from "./shared-components/loading-full-page/LoadingFullPage";
 
 const Dashboard = lazy(() => import("./components/dashboard/Dashboard"))
 const TopBar = lazy(() => import("./shared-components/top-bar/TopBar"));
@@ -12,35 +13,35 @@ function App() {
           <BrowserRouter>
               <Switch>
                   <Route path="//">
-                      <Suspense fallback={<div>Loading...</div>}>
+                      <Suspense fallback={<LoadingFullPage />}>
                           <TopBar />
                           <Login />
                       </Suspense>
                   </Route>
 
                   <Route path="/dashboard">
-                      <Suspense fallback={<div>Loading...</div>}>
+                      <Suspense fallback={<LoadingFullPage />}>
                           <TopBar />
                           <Dashboard />
                       </Suspense>
                   </Route>
 
                   <Route path="/update">
-                      <Suspense fallback={<div>Loading...</div>}>
+                      <Suspense fallback={<LoadingFullPage />}>
                           <TopBar />
                           Forms to update data
                       </Suspense>
                   </Route>
 
                   <Route path="/graphs">
-                      <Suspense fallback={<div>Loading...</div>}>
+                      <Suspense fallback={<LoadingFullPage />}>
                           <TopBar />
                           Graphs of historical data
                       </Suspense>
                   </Route>
 
                   <Route path="*">
-                      404
+                      <h1>Error 404</h1>
                   </Route>
               </Switch>
           </BrowserRouter>
